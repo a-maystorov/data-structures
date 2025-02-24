1626bf43524f867241772646431a9c1e766bfe26
a-maystorov <alkin971@gmail.com>
2025-02-24 13:20:38 +0100

    add solution for first non repeating char

--- java/src/Main.java
+++ java/src/Main.java
@@ -3,14 +3,6 @@ import java.util.Queue;
 
 public class Main {
     public static void main(String[] args) {
-        PrioQueue queue = new PrioQueue();
-        queue.add(5);
-        queue.add(3);
-        queue.add(6);
-        queue.add(1);
-        queue.add(4);
-        System.out.println(queue);
-
-        while (!queue.isEmpty()) System.out.println(queue.remove());
+        System.out.println(FirstNonRepeatedChar.solution("a green apple"));
     }
 }
