a88446f38747e437851ae5de990caee0d50f51f8
a-maystorov <alkin971@gmail.com>
2025-02-24 11:36:38 +0100

    add basic array queue implementation

--- java/src/Main.java
+++ java/src/Main.java
@@ -3,11 +3,19 @@ import java.util.Queue;
 
 public class Main {
     public static void main(String[] args) {
-        Queue<Integer> queue = new ArrayDeque<>();
-        queue.add(10);
-        queue.add(20);
-        queue.add(30);
-        ReverseQueue.reverse(queue);
+        ArrayQueue queue = new ArrayQueue(5);
+        queue.enqueue(10);
+        queue.enqueue(20);
+        queue.enqueue(30);
+        queue.dequeue();
+        var front = queue.dequeue();
+        queue.enqueue(40);
+        queue.enqueue(50);
+        queue.enqueue(60);
+        queue.enqueue(70);
+        queue.dequeue();
+        queue.enqueue(80);
+        System.out.println(front);
         System.out.println(queue);
     }
 }
