7e78a7ffe481344f72225a870917ceaaa1d1f8c3
a-maystorov <alkin971@gmail.com>
2025-02-26 10:38:17 +0100

    add validate bst solution

--- java/src/Main.java
+++ java/src/Main.java
@@ -8,16 +8,7 @@ public class Main {
         tree.insert(6);
         tree.insert(8);
         tree.insert(10);
-
-        MyTree tree2 = new MyTree();
-        tree2.insert(7);
-        tree2.insert(4);
-        tree2.insert(9);
-        tree2.insert(1);
-        tree2.insert(6);
-        tree2.insert(8);
-        tree2.insert(10);
-
-        System.out.println(tree.equals(null));
+        tree.swapRoot();
+        System.out.println(tree.isBinarySearchTree());
     }
 }
