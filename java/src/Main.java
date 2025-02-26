28ddd03f8407a133627dba169ff86324d5dd8c62
a-maystorov <alkin971@gmail.com>
2025-02-26 10:27:44 +0100

    add tree equality check solution

--- java/src/Main.java
+++ java/src/Main.java
@@ -8,6 +8,16 @@ public class Main {
         tree.insert(6);
         tree.insert(8);
         tree.insert(10);
-        System.out.println(tree.min());
+
+        MyTree tree2 = new MyTree();
+        tree2.insert(7);
+        tree2.insert(4);
+        tree2.insert(9);
+        tree2.insert(1);
+        tree2.insert(6);
+        tree2.insert(8);
+        tree2.insert(10);
+
+        System.out.println(tree.equals(null));
     }
 }
