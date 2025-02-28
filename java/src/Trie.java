a17925570fc902205a9bcdf5bb6e4824259b8057
a-maystorov <alkin971@gmail.com>
2025-02-28 11:50:20 +0100

    add abstracted implementation via hashmap

--- java/src/Trie.java
+++ java/src/Trie.java
@@ -1,8 +1,11 @@
+import java.util.HashMap;
+
 public class Trie {
+    public static int ALPHABET_SIZE = 26;
+
     private static class Node {
-        public static int ALPHABET_SIZE = 26;
         private final char value;
-        private final Node[] children = new Node[ALPHABET_SIZE];
+        private final HashMap<Character, Node> children = new HashMap<>();
         private boolean isEndOfWord;
 
         public Node(char value) {
@@ -13,6 +16,18 @@ public class Trie {
         public String toString() {
             return "value=" + value;
         }
+
+        public boolean hasChild(char ch) {
+            return children.containsKey(ch);
+        }
+
+        public void addChild(char ch) {
+            children.put(ch, new Node(ch));
+        }
+
+        public Node getChild(char ch) {
+            return children.get(ch);
+        }
     }
 
     private final Node root = new Node(' ');
@@ -20,10 +35,9 @@ public class Trie {
     public void insert(String word) {
         var current = root;
         for (var ch: word.toCharArray()) {
-            var index = ch - 'a';
-            if (current.children[index] == null)
-                current.children[index] = new Node(ch);
-            current = current.children[index];
+            if (current.hasChild(ch))
+                current.addChild(ch);
+            current = current.getChild(ch);
         }
         current.isEndOfWord = true;
     }
