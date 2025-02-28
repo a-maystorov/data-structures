3a98acdc66cd97aaf20af70785e14795e767abec
a-maystorov <alkin971@gmail.com>
2025-02-28 11:02:41 +0100

    add heapify algorithm with optimal amount of recursions

--- java/src/Main.java
+++ java/src/Main.java
@@ -2,16 +2,8 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] numbers = { 5, 3, 10, 1, 4, 2 };
-        var heap = new Heap();
-
-        for (var number : numbers)
-            heap.insert(number);
-
-//        reverse loop direction for ascending order
-        for (int i = 0; i < numbers.length; i++)
-            numbers[i] = heap.remove();
-
+        int[] numbers = { 5, 3, 8, 4, 1, 2};
+        MaxHeap.heapify(numbers);
         System.out.println(Arrays.toString(numbers));
     }
 }
