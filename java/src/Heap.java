8e5e7104e4709166e6aca622f11056c55d213b89
a-maystorov <alkin971@gmail.com>
2025-02-28 10:23:51 +0100

    add heap class implementation

---
+++ java/src/Heap.java
@@ -0,0 +1,103 @@
+public class Heap {
+    private int[] items = new int[10];
+    private int size;
+
+    public void insert(int value) {
+        if (isFull())
+            throw new IllegalStateException();
+        items[size++] = value;
+        bubbleUp();
+    }
+
+    public int remove() {
+        if (isEmpty())
+            throw new IllegalStateException();
+
+        var root = items[0];
+        items[0] = items[--size];
+
+        bubbleDown();
+        return root;
+    }
+
+    private void bubbleDown() {
+        var index = 0;
+        while (index <= size && !isValidParent(index)) {
+            var largerChildIndex = largerChildIndex(index);
+            swap(index, largerChildIndex);
+            index = largerChildIndex;
+        }
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    private int largerChildIndex(int index) {
+        if (!hasLeftChild(index))
+            return index;
+
+        if (!hasRightChild(index))
+            return leftChildIndex(index);
+
+        return leftChild(index) > rightChild(index) ? leftChildIndex(index) : rightChildIndex(index);
+    }
+
+    private boolean hasLeftChild(int index) {
+        return leftChildIndex(index) <= size;
+    }
+
+    private boolean hasRightChild(int index) {
+        return rightChildIndex(index) <= size;
+    }
+
+    private boolean isValidParent(int index) {
+        if (!hasLeftChild(index))
+            return true;
+
+        var isValid = items[index] >= leftChild(index);
+
+        if (hasRightChild(index))
+            isValid &= items[index] >= rightChild(index);
+
+        return isValid;
+    }
+
+    private int rightChild(int index) {
+        return items[rightChildIndex(index)];
+    }
+
+    private int leftChild(int index) {
+        return items[leftChildIndex(index)];
+    }
+
+    private int leftChildIndex(int index) {
+        return index * 2 + 1;
+    }
+
+    private int rightChildIndex(int index) {
+        return index * 2 + 2;
+    }
+
+    public boolean isFull() {
+        return size == items.length;
+    }
+
+    private void bubbleUp() {
+        var lastItemIndex = size - 1;
+        while (lastItemIndex > 0 && items[lastItemIndex] > items[parent(lastItemIndex)]) {
+            swap(lastItemIndex, parent(lastItemIndex));
+            lastItemIndex = parent(lastItemIndex);
+        }
+    }
+
+    private int parent(int index) {
+        return (index - 1) / 2;
+    }
+
+    private void swap(int first, int second) {
+        var temp = items[first];
+        items[first] = items[second];
+        items[second] = temp;
+    }
+}
--- java/src/Main.java
+++ java/src/Main.java
@@ -1,8 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        AVLTree tree = new AVLTree();
-        tree.insert(10);
-        tree.insert(30);
-        tree.insert(20);
+        var heap = new Heap();
+        heap.insert(10);
+        heap.insert(5);
+        heap.insert(17);
+        heap.insert(4);
+        heap.insert(22);
+        heap.remove();
+        System.out.println("Done");
     }
 }
