2459054bc32df318eade22c4d81b85179167eb7e
a-maystorov <alkin971@gmail.com>
2025-02-28 12:15:10 +0100

    add trie delete solution

--- java/src/Main.java
+++ java/src/Main.java
@@ -3,7 +3,10 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         var trie = new Trie();
+        trie.insert("car");
         trie.insert("care");
-        trie.traverse();
+        trie.remove(null);
+        System.out.println(trie.contains("car"));
+        System.out.println(trie.contains("care"));
     }
 }
