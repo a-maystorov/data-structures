b1c670821b390fdc18c832d113909bfd2616bcd6
a-maystorov <alkin971@gmail.com>
2025-02-28 12:04:14 +0100

    add traversing a trie

--- java/src/Main.java
+++ java/src/Main.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         var trie = new Trie();
-        trie.insert("canada");
-        System.out.println(trie.contains("canada"));
+        trie.insert("care");
+        trie.traverse();
     }
 }
