a361ed9b1ef673ddfcde15946b44e223b6cbebec
a-maystorov <alkin971@gmail.com>
2025-02-28 10:41:54 +0100

    add heap sort solution

--- java/src/Main.java
+++ java/src/Main.java
@@ -1,12 +1,17 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
+        int[] numbers = { 5, 3, 10, 1, 4, 2 };
         var heap = new Heap();
-        heap.insert(10);
-        heap.insert(5);
-        heap.insert(17);
-        heap.insert(4);
-        heap.insert(22);
-        heap.remove();
-        System.out.println("Done");
+
+        for (var number : numbers)
+            heap.insert(number);
+
+//        reverse loop direction for ascending order
+        for (int i = 0; i < numbers.length; i++)
+            numbers[i] = heap.remove();
+
+        System.out.println(Arrays.toString(numbers));
     }
 }
