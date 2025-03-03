2fbd4be511c34976e8c537b030ba1e03b067feab
a-maystorov <alkin971@gmail.com>
2025-03-03 10:06:02 +0100

    refactor into oop solution

--- java/src/WeightedGraph.java
+++ java/src/WeightedGraph.java
@@ -4,8 +4,9 @@ import java.util.List;
 import java.util.Map;
 
 public class WeightedGraph {
-    private static class Node {
+    private class Node {
         private final String label;
+        private final List<Edge> edges = new ArrayList<>();
 
         public Node(String label) {
             this.label = label;
@@ -14,6 +15,14 @@ public class WeightedGraph {
         @Override
         public String toString() {
             return label;
+        }
+
+        public void addEdge(Node to, int weight) {
+            edges.add(new Edge(this, to, weight));
+        }
+
+        public List<Edge> getEdges() {
+            return edges;
         }
     }
 
@@ -35,12 +44,9 @@ public class WeightedGraph {
     }
 
     private final Map<String, Node> nodes = new HashMap<>();
-    private final Map<Node, List<Edge>> adjacencyList = new HashMap<>();
 
     public void addNode(String label) {
-        var node = new Node(label);
-        nodes.putIfAbsent(label, node);
-        adjacencyList.putIfAbsent(node, new ArrayList<>());
+        nodes.putIfAbsent(label, new Node(label));
     }
 
     public void addEdge(String from, String to, int weight) {
@@ -52,15 +58,15 @@ public class WeightedGraph {
         if (toNode == null)
             throw new IllegalArgumentException();
 
-        adjacencyList.get(fromNode).add(new Edge(fromNode, toNode, weight));
-        adjacencyList.get(toNode).add(new Edge(toNode, fromNode, weight));
+        fromNode.addEdge(toNode, weight);
+        toNode.addEdge(fromNode, weight);
     }
 
     public void print() {
-        for (var source : adjacencyList.keySet()) {
-            var targets = adjacencyList.get(source);
-            if (!targets.isEmpty())
-                System.out.println(source + " is connected to " + targets);
+        for (var node : nodes.values()) {
+            var edges = node.getEdges();
+            if (!edges.isEmpty())
+                System.out.println(node + " is connected to " + edges);
         }
     }
 }
