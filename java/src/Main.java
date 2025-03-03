76f0cb11dba8770171567f202afadc56cc06722a
a-maystorov <alkin971@gmail.com>
2025-03-03 17:46:10 +0100

    add jump search implementation

--- java/src/Main.java
+++ java/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         int[] numbers = { 1, 3, 5, 6, 7 };
         var search = new Search();
-        var index = search.ternarySearch(numbers, 3);
+        var index = search.jumpSearch(numbers, 0);
         System.out.println(index);
     }
 }
