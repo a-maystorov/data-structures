d6e652c7577f942bdda74063a6f859baa373dd7f
a-maystorov <alkin971@gmail.com>
2025-03-03 17:37:39 +0100

    add ternary search implementation

--- java/src/Main.java
+++ java/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         int[] numbers = { 1, 3, 5, 6, 7 };
         var search = new Search();
-        var index = search.binarySearch(numbers, 1);
+        var index = search.ternarySearch(numbers, 3);
         System.out.println(index);
     }
 }
