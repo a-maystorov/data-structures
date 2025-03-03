c375f1ef41961c46c39865bbc1087672d3539ba6
a-maystorov <alkin971@gmail.com>
2025-03-03 12:55:29 +0100

    add quick sort implementation

--- java/src/Main.java
+++ java/src/Main.java
@@ -2,8 +2,8 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] numbers = { 7, 3, 1, 4, 6, 2, 3 };
-        var sorter = new MergeSort();
+        int[] numbers = { 7, 3, 1, 5, 2 };
+        var sorter = new QuickSort();
         sorter.sort(numbers);
         System.out.println(Arrays.toString(numbers));
     }
