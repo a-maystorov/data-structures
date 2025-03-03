fb479c57691a472d62ec0cd02009748c61bfebda
a-maystorov <alkin971@gmail.com>
2025-03-03 17:28:13 +0100

    add recursive and iterative binary search implementations

--- java/src/Main.java
+++ java/src/Main.java
@@ -2,9 +2,9 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] numbers = { 7, 1, 3, 6, 5 };
+        int[] numbers = { 1, 3, 5, 6, 7 };
         var search = new Search();
-        var index = search.linearSearch(numbers, 3);
+        var index = search.binarySearch(numbers, 1);
         System.out.println(index);
     }
 }
