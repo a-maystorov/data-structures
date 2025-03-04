b561ac3a688c4208c34709d846700a94c6f293b3
a-maystorov <alkin971@gmail.com>
2025-03-04 09:47:03 +0100

    add count vowels solution

--- java/src/Main.java
+++ java/src/Main.java
@@ -2,9 +2,7 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] numbers = { 1, 3, 5, 6, 7 };
-        var search = new Search();
-        var index = search.exponentialSearch(numbers, 50);
-        System.out.println(index);
+        int count = StringUtils.countVowels("");
+        System.out.println(count);
     }
 }
