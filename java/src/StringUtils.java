1aafcb8608c29b87428cb1a9542db8c1bc0aa1c1
a-maystorov <alkin971@gmail.com>
2025-03-04 10:29:06 +0100

    add sentence capitalization solution

--- java/src/StringUtils.java
+++ java/src/StringUtils.java
@@ -84,4 +84,16 @@ public class StringUtils {
 
         return result;
     }
+
+    public static String capitalize(String sentence) {
+        if (sentence == null || sentence.trim().isEmpty())
+            return "";
+
+        String[] words = sentence.trim().replaceAll(" +", " ").split(" ");
+        for (int i = 0; i < words.length; i++) {
+            words[i] = words[i].substring(0, 1).toUpperCase() + words[i].substring(1).toLowerCase();
+        }
+
+        return String.join(" ", words);
+    }
 }
